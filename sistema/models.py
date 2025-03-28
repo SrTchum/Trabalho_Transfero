f9bd8e09cb25648dcb5824f9496caa5135dfb07b
Nathan <homegaminito@gmail.com>
2025-03-28 16:21:57 -0300

    first commit

--- sistema/models.py
+++ sistema/models.py
@@ -13,4 +13,24 @@ class Usuario(models.Model):
     endereco = models.CharField(max_length=100)
     data_cadastro = models.DateTimeField(default=timezone.now)
     ativo = models.BooleanField(default=True) 
-    # foto = 
+
+    def __str__(self):
+        return f'{self.nome} {self.sobrenome}'
+    
+class Filme(models.Model):
+    nome = models.CharField(max_length=50)
+    ano = models.DateField(default=timezone.now)
+    estudio = models.CharField(max_length=50)
+    genero = models.CharField(max_length=50)
+    sinopse = models.TextField()
+    data_cadastro = models.DateField(default=timezone.now)
+
+    def __str__(self):
+        return self.nome #toda vez que vc mensionar o objeto, ele vai mostrar o objeto por exemplo "carros 2" inves de "Filme Object (1)" para isso que serve
+
+class Genero(models.Model):
+    nome = models.CharField(max_length=50)
+    data_cadastro = models.DateField(default=timezone.now)
+
+    def __str__(self):
+        return self.nome
